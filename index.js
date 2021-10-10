7eedca9b23b6a61b0db75fb36285cdb0f23989c9
emre candan <wastorist@gmail.com>
2021-10-10 14:48:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const Koa = require("koa");
+const app = new Koa();
+
+// response
+app.use((ctx) => {
+  const url = ctx.url;
+
+  if (url === "/") {
+    ctx.body = "<h1>INDEX sayfasına hoş geldiniz </h1>";
+  } else if (url === "/hakkimizda") {
+    ctx.body = "<h1>Hakkımızda sayfasına hoş geldiniz </h1>";
+  } else if (url === "/iletisim") {
+    ctx.body = "<h1>İletişim sayfasına hoş geldiniz </h1>";
+  } else {
+    ctx.body = "<h1>404 sayfa bulunamadı. </h1>";
+  }
+});
+
+app.listen(3000);
